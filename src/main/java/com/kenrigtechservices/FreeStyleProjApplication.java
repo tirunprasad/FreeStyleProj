a0340f373038716e4c03b364ae061eaa2da27256
Tirun Prasad <tirunprasad27@gmail.com>
2022-08-09 12:35:29 +0530

    free style proj

---
+++ src/main/java/com/kenrigtechservices/FreeStyleProjApplication.java
@@ -0,0 +1,13 @@
+package com.kenrigtechservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeStyleProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeStyleProjApplication.class, args);
+	}
+
+}
